3097c8258b142d75f4bc3b70c12e19b6d1d6257b
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-05-12 12:52:35 +0600

    feat: implement start of the game

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -1,4 +1,7 @@
 // import './style.css';
+
+// Create tablo
+
 let counter = 100;
 let timer = 0;
 
@@ -22,24 +25,94 @@ timerBlock.classList.add('timer');
 timerBlock.innerText = timer;
 tablo.appendChild(timerBlock);
 
-const board = document.createElement('div');
-board.classList.add('board');
-document.body.appendChild(board);
+// Create board
 
-function createRow() {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  return row;
+let boardWidth = 10;
+let boardHeight = 10;
+let boardArea = boardWidth * boardHeight;
+
+function createBoard(boardWidth, boardHeight) {
+  const board = document.createElement('div');
+  board.classList.add('board');
+  board.setAttribute('id', 'board');
+  document.body.appendChild(board);
+  for (let i = 0; i < boardHeight; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    board.appendChild(row);
+    for (let j = 0; j < boardWidth; j++) {
+      let cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.setAttribute('id', `${i}-${j}`);    
+      row.appendChild(cell);
+    }
+  } 
 }
 
-for (let i = 0; i < 10; i++) {
-  const row = createRow();
-  board.appendChild(row);
-  for (let j = 0; j < 10; j++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.setAttribute('id', `${i}-${j}`);
-    cell.innerText = ' ';
-    row.appendChild(cell);
+createBoard(boardWidth, boardHeight)
+
+// Start the game
+
+let mineList;
+let totalMineAmount = 10;
+
+function mine(totalMineAmount) {
+  mineList = [];
+  for (let i = 0; i < totalMineAmount; i++) {
+    let mineX = Math.floor(Math.random() * boardWidth);
+    let mineY = Math.floor(Math.random() * boardHeight);
+    let mineCell = document.getElementById(`${mineX}-${mineY}`);
+    while (mineCell.classList.contains('mined')) {
+      mineX = Math.floor(Math.random() * boardWidth);
+      mineY = Math.floor(Math.random() * boardHeight);
+      mineCell = document.getElementById(`${mineX}-${mineY}`);
+    } 
+    mineCell.classList.add('mined');       
+    mineCell.innerHTML = '<span> &#10038; </span>';
+    mineList.push(`${mineX}-${mineY}`);
+  }  
+}
+
+const colorNumbers = {1: '#0000FF', 2: '#00FF00', 3: '#FF4500', 4: '#9932CC', 5: '#FFA500', 6: '#FFFF00', 7: '#FF1493', 8: '#00FFFF' }
+
+function calcMinesAround(mineList, boardWidth, boardHeight) { 
+  for (let i = 0; i < boardWidth; i++) {
+    for (let j = 0; j < boardHeight; j++) { 
+      const cell = document.getElementById(`${i}-${j}`);
+      if (cell.classList.contains('mined')) {
+        continue;
+      }  
+      let cellsAround = [`${i-1}-${j-1}`, `${i-1}-${j}`, `${i-1}-${j+1}`, 
+                         `${i}-${j-1}`, `${i}-${j+1}`,
+                         `${i+1}-${j-1}`, `${i+1}-${j}`, `${i+1}-${j+1}`];
+      const mineAround = mineList.filter(el => cellsAround.includes(el));  
+      const sumMineAround= mineAround.length;      
+      if (sumMineAround !== 0) {
+        cell.innerText = sumMineAround;      
+        cell.setAttribute('color', colorNumbers[sumMineAround]);   
+      } else {
+        cell.classList.add('empty');
+      }
+    }
+  }  
+}
+
+mine(totalMineAmount);
+calcMinesAround(mineList, boardWidth, boardHeight);
+
+// Game
+
+function mouseDown(event) {
+  const { target } = event;
+  if (target.classList.contains('cell')) {    
+    target.classList.add('opened'); 
+    if (target.classList.contains('mined')) {
+      alert('You lose');
+    }
   }
 }
+
+tablo.onmousedown = function (event) { mouseDown(event); };
+
+let board = document.getElementById('board');
+board.onmousedown = function (event) { mouseDown(event); };
