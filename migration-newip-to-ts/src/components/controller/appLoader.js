c4e4228a1be034f153f4c3fe2e96521f447bac86
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-06-09 20:40:56 +0600

    refactor: add API key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ee232a4ea84f416a9abb0a7e695b46ae', // получите свой ключ https://newsapi.org/
         });
     }
 }
