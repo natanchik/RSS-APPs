e80a7f0f1d533d1edd14855916e49e5017e18078
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-06-15 20:33:42 +0600

    init: start rss-css-selectors task

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'rss-css-selectors', 'src', 'index'),
+  mode: 'development',    
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'rss-css-selectors', 'dist'),
+    },
+    compress: true,    
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.ts$/i, 
+        use: 'ts-Loader',
+        exclude: /(node_modules)/,
+        include: [path.resolve(__dirname, 'rss-css-selectors', 'src')]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },            
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'rss-css-selectors', 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'rss-css-selectors', 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),        
+  ],
+};
