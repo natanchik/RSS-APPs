4f506301b86fea7cb13ed00b3c05d60c117b41a5
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-06-27 20:50:46 +0600

    feat: add selectors for check answer

--- rss-css-selectors/src/ts/data-levels.ts
+++ rss-css-selectors/src/ts/data-levels.ts
@@ -24,37 +24,37 @@ export const levelsData = [
   { // 3
     'header': 'Select the green book',
     'tableItems': [bookGreenAnim, napkin, book],
-    'cssCode': '#green',
+    'cssCode': ['#green', 'book#green'],
     'htmlCode': '<book id="green" /><napkin /><book />',
   },
   { // 4
     'header': 'Select the cat on the book',
     'tableItems': [napkin, [book, catAnim], cat],
-    'cssCode': 'book cat',
+    'cssCode': ['book cat', 'book > cat'],
     'htmlCode': '<napkin /><book><cat /></book><cat>',
   },
   { // 5
     'header': 'Select the letter on the green book',
     'tableItems': [[napkin, catOrange], [bookGreen, letterAnim], [book, letter]],
-    'cssCode': '#green letter',
+    'cssCode': ['#green letter', 'book#green letter', 'book#green > letter'],
     'htmlCode': '<napkin><cat class="orange" /</napkin><book id="green"><letter /></book><book><letter /></book>',
   },
   { // 6
     'header': 'Select the small orange cats',
     'tableItems': [catOrangeSmallAnim, cat, [napkin, catOrangeSmallAnim], [napkin, book]],
-    'cssCode': ['.small', 'cat.small', 'cat.orange'],
+    'cssCode': ['.small', 'cat.small', 'cat.orange', 'cat.orange.small', 'cat.small.orange'],
     'htmlCode': '<cat class="small" /><cat /><napkin><cat class="small" /></napkin><book /><napkin />',
   },
   { // 7
     'header': 'Select the small orange cats',
     'tableItems': [cat, catSmall, [napkin, catOrangeSmallAnim], [book, catOrange], [book, catOrangeSmallAnim]],
-    'cssCode': 'cat.orange.small',
+    'cssCode': ['cat.orange.small', 'cat.small.orange', '.small.orange', '.orange.small'],
     'htmlCode': '<cat /><cat class="small" /><napkin><cat class="orange small" /></napkin><book><cat class="orange" /><book /><book><cat class="orange small" /><book />',
   },
   { // 8
     'header': 'Select the small orange cats on the napkins',
     'tableItems': [[napkin, cat], catOrangeSmall, [napkin, catOrangeSmallAnim], [napkin, [book, catSmall]], [napkin, catOrangeSmallAnim]],
-    'cssCode': 'napkin cat.orange.small',
+    'cssCode': ['napkin cat.orange.small', 'napkin cat.orange', 'napkin .orange', 'napkin > cat.small', 'napkin > .small', 'napkin > cat.orange','napkin > .orange', 'napkin > cat.small.orange', 'napkin > cat.orange.small', 'napkin > .small.orange', 'napkin > .orange.small'],
     'htmlCode': '<napkin><cat /><napkin><cat class="orange small" /><napkin><cat class="orange small" /></napkin><napkin><cat class="small" /></napkin><napkin><cat class="orange small" /></napkin>',
   },
   { // 9
@@ -65,8 +65,9 @@ export const levelsData = [
   },
   { // 10
     'header': 'Select all the things',
-    'tableItems': [catAnim, [bookAnim, catOrangeSmall], napkinAnim, [napkinAnim, catOrangeAnim], [napkinAnim, catSmall], bookGreenAnim],
-    'cssCode': '*',
-    'htmlCode': '<cat /><book><cat class="orange small" /></book><napkin /><napkin><cat class="orange" /></napkin><napkin><cat class="small" /></napkin><book id="green" />',
+    'tableItems': [catAnim, [bookAnim, catOrangeSmall], napkinAnim, [napkinAnim, catOrangeAnim], [napkinAnim, catSmall]],
+    'cssCode': ['*', 'napkin, book, cat', 'napkin, cat, book', 'book, napkin, cat', 'book, cat, napkin', 'cat, book, napkin', 'cat, napkin, book'],
+    'htmlCode': '<cat /><book><cat class="orange small" /></book><napkin /><napkin><cat class="orange" /></napkin><napkin><cat class="small" /></napkin>',
   }
 ]
+
