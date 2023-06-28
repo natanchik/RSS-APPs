036039bf83cebf7befcd582b64ceeb0b4a47f991
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-06-29 00:21:58 +0600

    feat: implement func answerRight

--- rss-css-selectors/src/ts/reactions.ts
+++ rss-css-selectors/src/ts/reactions.ts
@@ -1,37 +1,21 @@
-export  const answerWrong = async function(editor: HTMLDivElement) {
-  editor.animate(
-  [
-    { transform: "translate(3px, 0)" },   
-    { transform: "translate(-3px, 0)"}
-  ],
-  {
+export async function answerWrong(editor: HTMLDivElement) {
+  editor.animate([{ transform: 'translate(3px, 0)' }, { transform: 'translate(-3px, 0)' }], {
     duration: 50,
     iterations: 5,
   });
 }
 
-export const answerRight = function() {
-  alert('You are right!');
-}
-
-const congratulations = document.createElement('pre');
-congratulations.classList.add('congratulations');
-congratulations.innerText = `Congratulations!!!
-You are winner!!!`;
-
-export const win = function(table: HTMLDivElement) {
-  table.innerHTML = '';
-  table.appendChild(congratulations);
-  table.animate(
-    [
-      { background: "repeating-linear-gradient(45deg, #00FF00, #FDE910, #00FF00, #FDE910)" },
-      { background: "repeating-linear-gradient(135deg, #FDE910, #00FF00, #FDE910, #00FF00)" },
-      { background: "repeating-linear-gradient(225deg, #00FF00, #FDE910, #00FF00, #FDE910)" },
-      { background: "repeating-linear-gradient(315deg, #FDE910, #00FF00, #FDE910, #00FF00)" },
-    ],
-    {
-      duration: 2000,
-      iterations: Infinity,            
-    }
-  );
+export async function answerRight() {
+  const wrapper = document.body.querySelector('.wrapper');
+  const textBlock = document.createElement('div');
+  textBlock.classList.add('textBlock');
+  const textYouAreRight = document.createElement('pre');
+  textYouAreRight.classList.add('textYouAreRight');
+  textYouAreRight.innerText = `You are right!
+  Next Level`;
+  textBlock.appendChild(textYouAreRight);
+  wrapper?.appendChild(textBlock);
+  setTimeout(() => {
+    wrapper?.removeChild(textBlock);
+  }, 1500);
 }
