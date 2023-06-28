e3c1d005a92064626228dc6108fd268d0665b500
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-06-28 19:06:35 +0600

    fix: fix links in footer

--- rss-css-selectors/src/ts/footer.ts
+++ rss-css-selectors/src/ts/footer.ts
@@ -2,12 +2,11 @@ export const footer = document.createElement('div');
 footer.classList.add('footer');
 
 export const logo = document.createElement('span');
-logo.classList.add('logo');
-logo.innerHTML = '<a href="https://rs.school/js/"></a>';
+logo.innerHTML = '<a href="https://rs.school/js/"><div class="logo"></div></a>';
 footer.appendChild(logo);
 
 const footerText = document.createElement('div');
 footerText.classList.add('footer-text');
-footerText.innerHTML = `<p><a href='https://github.com/rolling-scopes-school/natanchik-JSFE2023Q1/tree/rss-css-selectors'>My github repository</a></p>
+footerText.innerHTML = `<p>My <a href='https://github.com/rolling-scopes-school/natanchik-JSFE2023Q1/tree/rss-css-selectors'>github</a> repository</p>
 <p>2023</p>`;
 footer.appendChild(footerText);
