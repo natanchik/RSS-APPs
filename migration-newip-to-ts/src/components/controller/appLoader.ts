149d0f8f7647ad5dccf0ab6ecdbcc9d53f479787
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-07-03 16:52:20 +0600

    fix: delete comments

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,9 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
-      // https://rss-news-api.onrender.com/, https://news-proxy.spanb4.shop/
-      apiKey: 'ee232a4ea84f416a9abb0a7e695b46ae', // получите свой ключ https://newsapi.org/
+    super('https://newsapi.org/v2/', {      
+      apiKey: 'ee232a4ea84f416a9abb0a7e695b46ae',
     });
   }
 }
