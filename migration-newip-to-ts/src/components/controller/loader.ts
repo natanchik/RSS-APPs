7258935286331ddc1b423a9f19757598ac78e1b3
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-07-03 16:57:31 +0600

    refactor: refactor loader

--- migration-newip-to-ts/src/components/controller/loader.ts
+++ migration-newip-to-ts/src/components/controller/loader.ts
@@ -20,12 +20,10 @@ class Loader {
   }
 
   public errorHandler(res: Response) {
-    if (!res.ok) {
-      if (res.status === 401 || res.status === 404)
-        console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+    if (!res.ok && (res.status === 401 || res.status === 404)) {
+      console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
       throw Error(res.statusText);
     }
-
     return res;
   }
 
