51f2a4ecfa01c1489c7a43bfb2c68bc0a412aead
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-07-05 21:14:18 +0600

    refactor: destruct func 'show-levels'

--- rss-css-selectors/src/ts/funcs/show-levels.ts
+++ rss-css-selectors/src/ts/funcs/show-levels.ts
@@ -2,20 +2,18 @@ import { levelBlock, menuButton } from '../blocks/levels';
 
 const xShowLevels = { transform: 'translateX(0)' };
 const xHideLevels = { transform: 'translateX(200px)' };
-const optionsShowLevels = { duration: 2000, iterations: 1, }
+const optionsShowLevels = { duration: 2000, iterations: 1 };
+
+function animateLevelBlock() {
+  if (menuButton.classList.contains('pushed')) {
+    levelBlock.animate([xHideLevels, xShowLevels], optionsShowLevels);
+  } else {
+    levelBlock.animate([xShowLevels, xHideLevels], optionsShowLevels);
+  }
+}
 
 export function showLevels() {
   menuButton.classList.toggle('pushed');
-  if (menuButton.classList.contains('pushed')) {
-    levelBlock.animate(
-      [ xHideLevels, xShowLevels ], 
-      optionsShowLevels
-    );    
-  } else {
-    levelBlock.animate(
-      [ xShowLevels, xHideLevels ], 
-      optionsShowLevels
-    );
-  }  
   levelBlock.classList.toggle('pushed');
+  animateLevelBlock();
 }
