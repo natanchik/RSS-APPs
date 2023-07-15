60ad9c7f8df2e7a5c057d0a9618fa0044856e1e7
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-07-15 21:32:08 +0600

    feat: implement update car

---
+++ async-race/src/ts/requests/update-car.ts
@@ -0,0 +1,27 @@
+import { paintCar } from '../funcs/create/paint-car';
+
+export async function updateCar(raceStrip: HTMLDivElement) {
+  const colorBtn = document.querySelectorAll('.colorBtn')[1];
+  const markInput = document.querySelector('.inputBtn');
+  if (colorBtn && markInput && colorBtn instanceof HTMLInputElement && markInput instanceof HTMLInputElement) {
+    const data = { name: markInput.value, color: colorBtn.value };
+    fetch(`http://127.0.0.1:3000/garage/${raceStrip.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          const carImg = raceStrip.querySelector('.car-image');
+          if (carImg) {
+            carImg.innerHTML = paintCar(colorBtn.value);
+          }
+          const carName = raceStrip.querySelector('.car-name');
+          if (carName && carName instanceof HTMLParagraphElement) {
+            carName.innerText = markInput.value;
+          }
+        }
+      })
+      .catch((e) => `Something is wrong, error: ${e}`);
+  }
+}
