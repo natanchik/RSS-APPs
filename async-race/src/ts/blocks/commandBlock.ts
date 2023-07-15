720be52a6ac6cc78c04e68259a798463c02eb68c
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-07-15 21:33:55 +0600

    feat: add handlers

--- async-race/src/ts/blocks/commandBlock.ts
+++ async-race/src/ts/blocks/commandBlock.ts
@@ -2,57 +2,66 @@ import { createMarks } from '../funcs/create/create-marks';
 import { createColor } from '../funcs/create/create-color';
 import { createBtn } from '../funcs/create/create-btn';
 import { Marks } from '../marks';
+import { createCar } from '../requests/create-car';
+import { updateCar } from '../requests/update-car';
 
-export function createCommandBlock() {
-  const commandBlock = document.createElement('div');
-  commandBlock.classList.add('commandBlock');
+export const commandBlock = document.createElement('div');
+commandBlock.classList.add('commandBlock');
 
-  const commandRow1 = document.createElement('div');
-  commandRow1.classList.add('commandRow');
-  commandBlock.appendChild(commandRow1);
+const commandRow1 = document.createElement('div');
+commandRow1.classList.add('commandRow');
+commandBlock.appendChild(commandRow1);
 
-  createMarks(commandRow1, Marks);
+createMarks(commandRow1, Marks);
 
-  const btnColor = createColor();
-  btnColor.classList.add('commandItem');
-  commandRow1.appendChild(btnColor);
+const btnColor = createColor();
+btnColor.classList.add('commandItem');
+commandRow1.appendChild(btnColor);
 
-  const btnCreate = createBtn('create');
-  btnCreate.classList.add('commandItem');
-  commandRow1.appendChild(btnCreate);
+const btnCreate = createBtn('create');
+btnCreate.classList.add('commandItem');
+commandRow1.appendChild(btnCreate);
 
-  const commandRow2 = document.createElement('div');
-  commandRow2.classList.add('commandRow');
-  commandBlock.appendChild(commandRow2);
+btnCreate.addEventListener('click', () => {
+  createCar();
+});
 
-  const btnInput = document.createElement('input');
-  btnInput.classList.add('inputBtn');
-  btnInput.classList.add('commandItem');
-  commandRow2.appendChild(btnInput);
+const commandRow2 = document.createElement('div');
+commandRow2.classList.add('commandRow');
+commandBlock.appendChild(commandRow2);
 
-  const btnColor2 = createColor();
-  btnColor2.classList.add('commandItem');
-  commandRow2.appendChild(btnColor2);
+const btnInput = document.createElement('input');
+btnInput.classList.add('inputBtn');
+btnInput.classList.add('commandItem');
+commandRow2.appendChild(btnInput);
 
-  const BtnUpdate = createBtn('update');
-  BtnUpdate.classList.add('commandItem');
-  commandRow2.appendChild(BtnUpdate);
+const btnColor2 = createColor();
+btnColor2.classList.add('commandItem');
+commandRow2.appendChild(btnColor2);
 
-  const commandRow3 = document.createElement('div');
-  commandRow3.classList.add('commandRow');
-  commandBlock.appendChild(commandRow3);
+const btnUpdate = createBtn('update');
+btnUpdate.classList.add('commandItem');
+commandRow2.appendChild(btnUpdate);
 
-  const btnRace = createBtn('race');
-  btnRace.classList.add('commandItem');
-  commandRow3.appendChild(btnRace);
+btnUpdate.addEventListener('click', () => {
+  const activeCar = document.querySelector('.active');
+  if (activeCar && activeCar instanceof HTMLDivElement) {
+    updateCar(activeCar);
+  }
+});
 
-  const btnReset = createBtn('reset');
-  btnReset.classList.add('commandItem');
-  commandRow3.appendChild(btnReset);
+const commandRow3 = document.createElement('div');
+commandRow3.classList.add('commandRow');
+commandBlock.appendChild(commandRow3);
 
-  const btnGenerateCars = createBtn('generate cars');
-  btnGenerateCars.classList.add('commandItem');
-  commandRow3.appendChild(btnGenerateCars);
+const btnRace = createBtn('race');
+btnRace.classList.add('commandItem');
+commandRow3.appendChild(btnRace);
 
-  return commandBlock;
-}
+const btnReset = createBtn('reset');
+btnReset.classList.add('commandItem');
+commandRow3.appendChild(btnReset);
+
+const btnGenerateCars = createBtn('generate cars');
+btnGenerateCars.classList.add('commandItem');
+commandRow3.appendChild(btnGenerateCars);
--- async-race/src/ts/blocks/race-strip.ts
+++ async-race/src/ts/blocks/race-strip.ts
@@ -1,6 +1,8 @@
 import { createBtn, createBtnAB } from '../funcs/create/create-btn';
 import { drawCar } from '../funcs/create/draw-car';
 import { drawFlag } from '../funcs/create/draw-flag';
+import { selectCar } from '../funcs/select-car';
+import { deleteCar } from '../requests/delete-car';
 
 function createRaceStripRowUp(name: string) {
   const raceStripRow = document.createElement('div');
@@ -23,7 +25,7 @@ function createRaceStripRowUp(name: string) {
   return raceStripRow;
 }
 
-function createRaceStripRowCar(color: string, id: number) {
+function createRaceStripRowCar(color: string) {
   const raceStripRow = document.createElement('div');
   raceStripRow.classList.add('race-strip-row');
 
@@ -33,18 +35,34 @@ function createRaceStripRowCar(color: string, id: number) {
   abBlock.appendChild(createBtnAB('b'));
 
   raceStripRow.appendChild(abBlock);
-  raceStripRow.appendChild(drawCar(color, id));
+  raceStripRow.appendChild(drawCar(color));
   raceStripRow.appendChild(drawFlag());
 
   return raceStripRow;
 }
 
+function handlerRaceStrip(event: Event, raceStrip: HTMLDivElement) {
+  const { target } = event;
+  if (target && target instanceof HTMLButtonElement) {
+    if (target.classList.contains('selectBtn')) {
+      selectCar(raceStrip);
+    } else if (target.classList.contains('removeBtn')) {
+      deleteCar(raceStrip);
+    }
+  }
+}
+
 export function createRaceStrip(name: string, color: string, id: number) {
   const raceStrip = document.createElement('div');
   raceStrip.classList.add('race-strip');
+  raceStrip.setAttribute('id', `${id}`);
 
-  document.body.appendChild(createRaceStripRowUp(name));
-  document.body.appendChild(createRaceStripRowCar(color, id));
+  raceStrip.appendChild(createRaceStripRowUp(name));
+  raceStrip.appendChild(createRaceStripRowCar(color));
+
+  raceStrip.addEventListener('click', (event) => {
+    handlerRaceStrip(event, raceStrip);
+  });
 
   return raceStrip;
 }
